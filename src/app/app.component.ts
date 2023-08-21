e4ec903d99de1ab4ee823081007523609ce28aac
sp90 <dev@nomis.dk>
2023-08-21 23:23:30 +0200

    Update nomis blog terminal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommandsState } from './commands.state';
+
+const fb = new FormBuilder();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nomis-terminal';
+export class AppComponent implements OnInit {
+  @ViewChild('textarea') textarea!: HTMLTextAreaElement;
+
+  cmdField = fb.control('');
+  cmdHistory = this.cmdState.cmdHistory.asReadonly();
+  contentStream = this.cmdState.contentStream.asReadonly();
+
+  constructor(private cmdState: CommandsState) {}
+
+  ngOnInit(): void {
+    this.textarea?.focus();
+    this.cmdState.runCmd('help');
+  }
+
+  submitCmd($event: Event) {
+    $event.preventDefault();
+
+    this.cmdState.runCmd(this.cmdField.value as string);
+    this.cmdField.setValue('');
+  }
 }
---
+++ src/app/commands.state.ts
@@ -0,0 +1,87 @@
+import { Injectable, signal } from '@angular/core';
+
+type KeyValuePair = { [key: string]: Function };
+
+// <b>cd</b>      change directory
+const helpText = `
+  <b>read</b>    read a blog post
+  <b>ls</b>      List blog posts
+
+  <b class="gr">help</b>    Print this help menu
+`;
+
+// Create sub help commands for each command
+const CMD_TREE = {
+  // cd: (_: string, dir: string) => {
+  //   // Preload all blog posts
+  //   // Figure out if if it exists as blog post
+
+  //   if (false) {
+  //     return 'cd: not a directory: ' + dir;
+  //   }
+
+  //   return 'cd: no such file or directory: ' + dir;
+  // },
+  ls: () => {
+    // Preload all blog posts
+    // Show a list of blog post slugs
+  },
+  empty: () => '',
+  cmdNotFound: (cmd: string) => 'command not found: ' + cmd,
+  help: () => helpText
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommandsState {
+  private statusStr = '<b class="t">Nomis</b> via 🅰️ <b class="r">v16.2.1</b>';
+
+  cmdHistory = signal<string[]>([]);
+  contentStream = signal<string>('');
+
+  runCmd(cmdString: string, noHistory = false) {
+    if (noHistory === false) {
+      this.cmdHistory.set([...this.cmdHistory(), cmdString]);
+    }
+
+    const cmdSplit = cmdString.split(' ');
+    const cmd = cmdSplit[0];
+    const cmdFn =
+      cmd === '' ? CMD_TREE['empty'] : (CMD_TREE as KeyValuePair)[cmd] || CMD_TREE['cmdNotFound'];
+    const result = cmdFn(cmdString, ...cmdSplit.splice(1));
+
+    this.addNewContent(result, cmd === 'help' ? undefined : cmdString);
+
+    return result;
+  }
+
+  private addNewContent(content: string, cmdString?: string) {
+    const currentContent = this.contentStream();
+    let contentStr = '';
+
+    if (currentContent === '') {
+      contentStr += this.statusStr + '\n';
+
+      if (cmdString) {
+        contentStr += cmdString + '\n';
+      }
+
+      contentStr += content + '\n';
+
+      this.contentStream.set(contentStr);
+      return;
+    }
+
+    contentStr += currentContent + '\n';
+    contentStr += this.statusStr + '\n';
+
+    if (cmdString) {
+      contentStr += '<b class="r">❯</b> ' + cmdString + '\n';
+    }
+
+    contentStr += content + '\n';
+
+    this.contentStream.set(contentStr);
+  }
+}
