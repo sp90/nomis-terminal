cc4f885494ce7626b4f961a8c2f69a68e754d1db
sp90 <dev@nomis.dk>
2023-09-12 22:31:41 +0200

    Convert to mjs

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,4 @@
 // const server = require('../dist/nomis-terminal/server/main');
-import server from '../dist/nomis-terminal/server/main';
+import server from '../dist/nomis-terminal/server/main.js';
 
 export default server.app();
