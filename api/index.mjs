78a9fcb21283f6ae1c6469c1b2440ad6d7b67f12
sp90 <dev@nomis.dk>
2023-09-14 09:55:34 +0200

    Update build

--- api/index.mjs
+++
@@ -1,4 +0,0 @@
-// const server = require('../dist/nomis-terminal/server/main');
-import server from '../dist/nomis-terminal/server/main.js';
-
-export default server.app();
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideClientHydration()
   ]
 };
